692b1f76e14ec6cb0d1c4f35941ca92fed2c71e8
배재훈 <lange50300@naver.com>
2024-11-02 02:44:03 +0900

    third commit

--- TestProject/TestProject/src/main/java/com/example/testproject/dto/MemberDTO.java
+++ TestProject/TestProject/src/main/java/com/example/testproject/dto/MemberDTO.java
@@ -1,41 +1,16 @@
 package com.example.testproject.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class MemberDTO {
 
     private String name;
     private String email;
     private String org;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getOrg() {
-        return org;
-    }
-
-    public void setOrg(String org) {
-        this.org = org;
-    }
-
-    @Override
-    public String toString() {
-        return "MemberDTO{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", org='" + org + '\'' +
-                '}';
-    }
 }

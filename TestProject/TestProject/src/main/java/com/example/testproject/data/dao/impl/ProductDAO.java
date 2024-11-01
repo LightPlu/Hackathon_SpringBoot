63022e7f97df62cf43cd569b07f0b02bedc9650a
배재훈 <lange50300@naver.com>
2024-11-02 02:45:35 +0900

    fourth commit

---
+++ TestProject/TestProject/src/main/java/com/example/testproject/data/dao/impl/ProductDAO.java
@@ -0,0 +1,10 @@
+package com.example.testproject.data.dao.impl;
+
+import com.example.testproject.data.entity.ProductEntity;
+
+public interface ProductDAO {
+
+    ProductEntity saveProduct(ProductEntity productEntity);
+
+    ProductEntity getProduct(String productId);
+}
